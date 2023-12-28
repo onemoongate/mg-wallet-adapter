9476952ba38a44a39d4f3940ccadb14793dbc928
ZeroTimeDrift <403.hevar@gmail.com>
2023-12-28 15:12:50 +0300

    Update ContextProvider.tsx

--- packages/starter/example/src/components/ContextProvider.tsx
+++ packages/starter/example/src/components/ContextProvider.tsx
@@ -6,7 +6,7 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { WalletDialogProvider as MaterialUIWalletDialogProvider } from '@solana/wallet-adapter-material-ui';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletModalProvider as ReactUIWalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { MoongateWalletAdapter } from '@moongate/moongate-adapter';
+import { MoongateWalletAdapter } from '@zerotimedrift/moongate-adapter';
 import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { type SolanaSignInInput } from '@solana/wallet-standard-features';
 import { verifySignIn } from '@solana/wallet-standard-util';
